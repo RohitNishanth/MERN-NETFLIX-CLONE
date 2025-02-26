2be1c2aa7ddd7127d3e587e658453f2cf4380255
Rohit Nishanth P <rohitns2003p@gmail.com>
2025-02-26 12:16:02 +0530

    new Commit

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = ENV_VARS.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json()); // will allow us to parse req.body
+app.use(express.json()); 
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
